792b349ffccdfb63aea16ea482361e431f7e75c0
Aryainguz <aryaninguz369@gmail.com>
2024-01-09 10:20:46 +0530

    added Navbar and Hero components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import FeaturedProducts from '../components/FeaturedProducts'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
   <>
+  <Hero/>
   <FeaturedProducts/>
   </>
   )
