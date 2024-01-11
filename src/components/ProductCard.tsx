dae8ce3aae46f8eff00df070eca067b7cc03ef9b
Aryainguz <aryaninguz369@gmail.com>
2024-01-11 13:21:52 +0530

    added error compoenen

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,7 +10,7 @@ interface cardProps{
 const ProductCard:React.FC<cardProps> = ({title,price,image}) => {
   return (
     <>
-      <div className="relative group">
+      <div className="relative group cursor-pointer">
         <div className="overflow-hidden aspect-w-1 aspect-h-1">
           <img
             className="object-cover w-full h-full transition-all duration-300 group-hover:scale-125"
