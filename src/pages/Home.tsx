4e70f01e61d5d244dc8f5b56fc43f2fcc5bf5bf0
Aryainguz <aryaninguz369@gmail.com>
2024-02-14 11:19:10 +0530

    Update product component and add TopProducts page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,14 @@ import CTA from '../components/CTA'
 import ContactCTA from '../components/ContactCTA'
 import CustomersCTA from '../components/CustomersCTA'
 import Hero from '../components/Hero'
-import Products from './Hoodies'
+import TopProducts from './TopProducts'
 
 const Home = () => {
   return (
   <>
   <Hero/>
   <CustomersCTA/>
-  <Products/>
+  <TopProducts/>
   <CTA/>
   <ContactCTA/>
   </>
---
+++ src/pages/TopProducts.tsx
@@ -0,0 +1,49 @@
+import ProductsComponent from "../components/ProductsComponent"
+
+type Product = {
+  title:string,
+  price:number,
+  image:string,
+  bestseller?:boolean,
+  productid:number
+}
+
+
+const data:Product[] = [
+  {
+    productid: 1,
+    title: "Nike Air Force 1 '07",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D",
+
+  },
+  {
+    productid: 2,
+    title: "Nike Air Force 1 '07",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    productid: 3,
+    title: "Nike Air Force 1 '07",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    productid: 4,
+    title: "Nike Air Force 1 '07",
+    price: 90,
+    image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8fDA%3D",
+    bestseller:true
+  },
+]
+
+const TopProducts = () => {
+  return (
+    <>
+    <ProductsComponent heading="Best Sellers" tagline="Explore Our Best Selling Collection" description="This is our best selling collection from Zara to Gucci all but best sellings" data={data}/>
+    </>
+  )
+}
+
+export default TopProducts
